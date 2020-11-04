b5153327aa8c648412875c13ad6ef1923a30587a
Matt <tlach007@umn.edu>
2020-11-03 19:36:27 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Project code for Think Deep Learning (csci5980) at the University of Minnesota.
+
+To install:
+`
+pip install -e .
+`
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nlp-waypoints',
+      version='0.0.1',
+      python_requires='>=3',
+      install_requires=[
+        'numpy'
+      ]
+)
